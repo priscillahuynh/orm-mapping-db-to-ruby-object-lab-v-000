03cf6fa1f6044a73bd07a6cfa9105402d614a133
Priscilla H <priscillamhuynh@gmail.com>
2018-12-28 10:32:13 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,11 +60,13 @@ class Student
   end
 
   def all_students_in_grade_9
-    sql = <<-SQL 
-    SELECT * 
-    FROM students 
+    sql = <<-SQL
+    SELECT *
+    FROM students
     WHERE grade = 9
-    SQL 
-    
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db
   end
 end
