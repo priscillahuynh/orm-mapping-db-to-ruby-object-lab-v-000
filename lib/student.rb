e73c19d66da6f8888d726a9f07380aa798b0f6cb
Priscilla H <priscillamhuynh@gmail.com>
2018-12-28 10:34:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -82,4 +82,15 @@ class Student
       self.new_from_db(row)
     end
   end
+
+  def self.first_X_students_in_grade_10(x)
+    sql = <<-SQL
+    SELECT *
+    FROM students
+    WHERE grade < 12
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+  end
 end
